84c3638433674b6b22c9bfd2e7c91c7e25a12c69
JasperZwiers <jasper.zwiers16@gmail.com>
2018-11-05 10:28:57 +0100

    Niels: Styling vragenlijst

--- coreui-free-react-admin-template-master/src/views/Ndc/Vragenlijst/Vragenlijst.js
+++ coreui-free-react-admin-template-master/src/views/Ndc/Vragenlijst/Vragenlijst.js
@@ -11,9 +11,93 @@ class Vragenlijst extends Component {
                     <div className="card-body">
                         <div class="vragenlijst">
                             <table width="100%" class="table table-bordered table-striped table-hover" id="dataTables-example">
+                            <thead>
+                                    <tr>
+                                        <th colspan="7">0.Ik kan een maaltijd klaar maken</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="gradeA">
+                                        <td >
+                                            <center><img src="../../../../assets/img/Smileyrood.png" height="40px" width="45px" position="text-center" /></center>
+                                        </td>
+                                        <td class="center">Helemaal oneens
+                                    <div>
+                                        <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
+                                        </td>
+                                        
+                                        <td class="center">Oneens
+                                    <div>
+                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
+                                        </td>
+                                    
+                                        <td class="center">Neutraal
+                                    <div>
+                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
+                                        </td>
+                                        <td class="center">Eens
+                                    <div>
+                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
+                                        </td>
+                                        <td class="center">Helemaal mee eens
+                                    <div>
+                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
+                                        </td>
+                                        <td class="center">
+                                            <center><img src="../../../../assets/img/Smileygroen.png" height="40px" width="45px" position="text-center" /></center>
+                                        </td>
+                                    </tr>
+                                </tbody>
                                 <thead>
                                     <tr>
                                         <th colspan="7">1.Ik weet waar ik goed in ben namelijk:</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <tr className="gradeA">
+                                    <td class>
+                                        <center><img src="../../../../assets/img/Smileyrood.png" height="40px" width="45px" position="text-center" /></center>
+                                    </td>
+                                    <td class="center">Helemaal oneens
+                                <div>
+                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                </div>
+                                    </td>
+                                    
+                                    <td class="center">Oneens
+                                <div>
+                                <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                </div>
+                                    </td>
+                                
+                                    <td class="center">Neutraal
+                                <div>
+                                <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                </div>
+                                    </td>
+                                    <td class="center">Eens
+                                <div>
+                                <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                </div>
+                                    </td>
+                                    <td class="center">Helemaal mee eens
+                                <div>
+                                <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                </div>
+                                    </td>
+                                    <td class="center">
+                                        <center><img src="../../../../assets/img/Smileygroen.png" height="40px" width="45px" position="text-center" /></center>
+                                    </td>
+                                </tr>
+                            </tbody>
+                                <thead >
+                                    <tr>
+                                        <th colspan="7">2.Waar ik goed in ben kan ik gebruiken voor mijn studie of mijn werk</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -22,49 +106,31 @@ class Vragenlijst extends Component {
                                             <center><img src="../../../../assets/img/Smileyrood.png" height="40px" width="45px" position="text-center" /></center>
                                         </td>
                                         <td class="center">Helemaal oneens
-                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    <div>
+                                        <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
                                         </td>
+                                        
                                         <td class="center">Oneens
+                                    <div>
                                     <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
                                         </td>
+                                    
                                         <td class="center">Neutraal
+                                    <div>
                                     <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
                                         </td>
                                         <td class="center">Eens
+                                    <div>
                                     <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
                                         </td>
                                         <td class="center">Helemaal mee eens
+                                    <div>
                                     <input type="checkbox" name="Helemaaloneens" value="meting" />
-                                        </td>
-                                        <td class="center">
-                                            <center><img src="../../../../assets/img/Smileygroen.png" height="40px" width="45px" position="text-center" /></center>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                                <thead >
-                                    <tr>
-                                        <th colspan="7">2.Waar ik goed in ben kan ik gebruiken voor mijn studie of mijn werk</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="gradeA">
-                                        <td class>
-                                            <center><img src="../../../../assets/img/Smileyrood.png" height="40px" width="45px" position="text-center" /></center>
-                                        </td>
-                                        <td class="center">Helemaal oneens
-                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
-                                        </td>
-                                        <td class="center">Oneens
-                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
-                                        </td>
-                                        <td class="center">Neutraal
-                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
-                                        </td>
-                                        <td class="center">Eens
-                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
-                                        </td>
-                                        <td class="center">Helemaal mee eens
-                                    <input type="checkbox" name="Helemaaloneens" value="meting" />
+                                    </div>
                                         </td>
                                         <td class="center">
                                             <center><img src="../../../../assets/img/Smileygroen.png" height="40px" width="45px" position="text-center" /></center>
