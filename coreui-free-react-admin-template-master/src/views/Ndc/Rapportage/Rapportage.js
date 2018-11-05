48e7f57c5ad78bd4fb28af21aa3870f0aa82ae5c
JasperZwiers <jasper.zwiers16@gmail.com>
2018-11-05 09:35:00 +0100

    Jasper: Rapportage converted to react

---
+++ coreui-free-react-admin-template-master/src/views/Ndc/Rapportage/Rapportage.js
@@ -0,0 +1,211 @@
+import React, { Component } from 'react'
+
+class Rapportage extends Component {
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <div className="row">
+                    <div className="col-3">
+                        <div className="card card-filters">
+                            <div className="card-header">
+                                Filters
+                            </div>
+                            <div className="card-body">
+                                <div className="deelnemer-card">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-9">
+                        <div className="card card-rapportage">
+                            <div className="card-header">
+                                Rapportage
+                            </div>
+                            <div className="card-body">
+                                <table width="50%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>Deelnemer</th>
+                                        <th>Voornaam</th>
+                                        <th>Achternaam</th>
+                                        <th>leeftijd</th>
+                                        <th>locatie</th>
+                                        <th>Vooruitgang</th>
+                                        <th>Subsidiabel</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="odd gradeX">
+                                        <td>0001</td>
+                                        <td>Johan</td>
+                                        <td>van Hetschip</td>
+                                        <td class="center">48</td>
+                                        <td class="center">Utrecht</td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr class="even gradeC">
+                                        <td>0002</td>
+                                        <td>Erica</td>
+                                        <td>Peterskerk</td>
+                                        <td class="center">53</td>
+                                        <td class="center">Amsterdam</td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="odd gradeX">
+                                            <td>0001</td>
+                                            <td>Johan</td>
+                                            <td>van Hetschip</td>
+                                            <td class="center">48</td>
+                                            <td class="center">Utrecht</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <td>0002</td>
+                                            <td>Erica</td>
+                                            <td>Peterskerk</td>
+                                            <td class="center">53</td>
+                                            <td class="center">Amsterdam</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Rapportage
